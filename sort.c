9864f91d8518d8ab6c7fcf840e659f51e2dc0ba5
fujina <37600009+fujina@users.noreply.github.com>
2018-03-28 06:48:28 +0800

    3.28
    3.28

---
+++ sort.c
@@ -0,0 +1,34 @@
+﻿/*sort.c
+*输入三个整数，按大小顺序把它们输出
+*/
+
+#include<stdio.h>
+
+int main() {
+	int intSort[3];
+	printf("请输入三个整数：\n");
+	scanf("%d%d%d",&intSort[0],&intSort[1],&intSort[2]);
+	
+		int temp;
+	if(intSort[0]<intSort[1])
+		{
+			temp=intSort[0];
+			intSort[0]=intSort[1];
+			intSort[1]=temp;
+		}
+	if(intSort[0]<intSort[2])
+	{
+		temp=intSort[0];
+		intSort[0]=intSort[2];
+		intSort[2]=temp;
+	}
+	if(intSort[1]<intSort[2])
+	{
+		temp=intSort[1];
+		intSort[1]=intSort[2];
+		intSort[2]=temp;
+	}
+	
+	printf("从大到小排序：%d ， %d , %d \n\n", intSort[0],intSort[1],intSort[2]);
+	return 0;
+}
