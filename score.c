fd4f89e258cda1b88cd8cf0a4771fb5ede3bfd11
fujina <37600009+fujina@users.noreply.github.com>
2018-05-02 13:43:01 +0800

    5.2
    5.2

---
+++ score.c
@@ -0,0 +1,31 @@
+﻿//8. 给出一百分制成绩，
+//要求输出成绩等级'A'、 'B'、 'C'、 'D'、 'E'。
+//90分以上为'A'，80~89分为'B'，70~79分为'C'，
+//60~69分为'D'，60分以下为'E'。
+
+#include<stdio.h>
+
+int main()
+{
+int score;
+char level;
+do{
+	printf("Please input your score(0~100): ");
+	scanf("%d",&score);
+}while(score>100 || score<0);
+
+if(score>=90)
+	level = 'A';
+if(score<90)
+	level = 'B';
+if(score<80)
+	level = 'C';
+if(score<70)
+	level = 'D';
+if(score<60)
+	level = 'E';
+
+printf("your score level is : %c\n" , level);
+return 0;
+
+}
---
+++ week6.c
@@ -0,0 +1,16 @@
+﻿# include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("enter x:");
+	scanf("%d",&x);
+	y=-1;				//先假设 x < 0, y = -1
+	if(x!=0)			//如果x 不等于 0
+		if(x>0)			//如果 x > 0 , y = 1
+			y=1;
+		else
+			y=0;		//否则 x < 0 ，则y也等于0  这里不符合解题要求
+						//如果 x = 0, y 等于前面默认的-1, 不符合解题要求
+	printf("x=%d,y=%d\n",x,y);
+	return 0;
+}
